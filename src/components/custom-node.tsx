0470bd34d4d42fd5a7e2497e0401dda8ea1c26c5
aqum0001 <aqum0001@student.monash.edu>
2025-05-03 03:32:35 +0800

    Added logo images

--- src/components/custom-node.tsx
+++ src/components/custom-node.tsx
@@ -1,4 +1,5 @@
 import { Handle, Position, type NodeProps } from "reactflow"
+import Image from "next/image"
 
 type NodeData = {
   label: string
@@ -13,7 +14,12 @@ export function CustomNode({ data, isConnectable }: NodeProps<NodeData>) {
 
   return (
     <div className={nodeClass}>
-      <div className="custom-node-header">{data.label}</div>
+      {data.label !== "SOL Wallet" && (
+      <div className="custom-node-header"><Image src={`/PNG/${data.label.toLowerCase()}-logo.png`} alt={`${data.label} logo`} width={24} height={24} />{data.label}</div>
+      )}
+      {data.label === "SOL Wallet" && (
+      <div className="custom-node-header"><Image src={`/PNG/sol-logo.png`} alt={`${data.label} logo`} width={24} height={24} />{data.label}</div>
+      )}
       {data.description && <div className="custom-node-content">{data.description}</div>}
       {data.percentage && <div className="custom-node-content">{data.percentage}</div>}
       <Handle type="source" position={Position.Right} isConnectable={isConnectable} />
