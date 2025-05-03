438472fb5ff6fafd681f45df560262a22e5206f2
aqum0001 <aqum0001@student.monash.edu>
2025-05-03 12:00:24 +0800

    Aligned logo with label

--- src/components/custom-node.tsx
+++ src/components/custom-node.tsx
@@ -14,18 +14,21 @@ export function CustomNode({ data, isConnectable }: NodeProps<NodeData>) {
 
   return (
     <div className={nodeClass}>
-      {data.label !== "SOL Wallet" && (
-      <div className="custom-node-header"><Image src={`/PNG/${data.label.toLowerCase()}-logo.png`} alt={`${data.label} logo`} width={24} height={24} />{data.label}</div>
-      )}
-      {data.label === "SOL Wallet" && (
-      <div className="custom-node-header"><Image src={`/PNG/sol-logo.png`} alt={`${data.label} logo`} width={24} height={24} />{data.label}</div>
-      )}
+      <div className="custom-node-header" style={{ display: "flex", alignItems: "center", gap: "8px" }}>
+      <Image
+        src={data.label === "SOL Wallet" ? `/PNG/sol-logo.png` : `/PNG/${data.label.toLowerCase()}-logo.png`}
+        alt={`${data.label} logo`}
+        width={24}
+        height={24}
+      />
+      {data.label}
+      </div>
       {data.description && <div className="custom-node-content">{data.description}</div>}
       {data.percentage && <div className="custom-node-content">{data.percentage}</div>}
       <Handle type="source" position={Position.Right} isConnectable={isConnectable} />
       {/* Render handle only if label of node isn't "SOL Wallet" */}
       {data.label !== "SOL Wallet" && (
-        <Handle type="target" position={Position.Left} isConnectable={isConnectable} />
+      <Handle type="target" position={Position.Left} isConnectable={isConnectable} />
       )}
     </div>
   )
