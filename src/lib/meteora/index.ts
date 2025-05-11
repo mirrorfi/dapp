9637ead9550aca45bf4542103375245c25e0cacc
Noobmaster169 <mariotaning@gmail.com>
2025-05-11 21:38:35 +0800

    Update Meteora Tool

--- src/lib/meteora/index.ts
+++ src/lib/meteora/index.ts
@@ -29,48 +29,77 @@ export async function getAllUserPositions(userPublicKey: PublicKey) {
         
         const tokenX = position.tokenX;
         const tokenY = position.tokenY;
-        const positionsData = position.lbPairPositionsData;
+        const tokenX_info_res:any = await fetch(`https://api.solana.fm/v0/tokens/${tokenX.mint.address.toString()}`);
+        const tokenY_info_res:any = await fetch(`https://api.solana.fm/v0/tokens/${tokenY.mint.address.toString()}`);
+        const tokenX_info_json = await tokenX_info_res.json();
+        const tokenY_info_json = await tokenY_info_res.json();
+        const tokenX_info = tokenX_info_json.result.data;
+        const tokenY_info = tokenY_info_json.result.data;
+        console.log("Token X Info:", tokenX_info);
+        console.log("Token Y Info:", tokenY_info);
 
+        const tokenX_symbol = tokenX_info.symbol === "wSOL" ? "SOL" : tokenX_info.symbol;
+        const tokenY_symbol = tokenY_info.symbol === "wSOL" ? "SOL" : tokenY_info.symbol;
+
+        const price_res = await fetch(`https://lite-api.jup.ag/price/v2?ids=${tokenX.mint.address.toString()},${tokenY.mint.address.toString()}`);
+        const price_data = await price_res.json();
+        const priceX = Number(price_data.data[tokenX.mint.address.toString()].price);
+        const priceY = Number(price_data.data[tokenY.mint.address.toString()].price);
+
+        console.log("Price X:", priceX);
+        console.log("Price Y:", priceY);
+
+
+        const positionsData = position.lbPairPositionsData;
         positionsData.forEach((data: any) => {
             const positionData = data.positionData;
-            console.log("Token X:", tokenX);
-            console.log("Token Y:", tokenY);
-            console.log("Token X Mint:", tokenX.mint.address.toString());
-            console.log("Token Y Mint:", tokenY.mint.address.toString());
-            console.log("Token X Decimal:", tokenX.mint.decimals);
-            console.log("Token Y Decimal:", tokenY.mint.decimals);
+            // console.log("Token X:", tokenX);
+            // console.log("Token Y:", tokenY);
+            // console.log("Token X Mint:", tokenX.mint.address.toString());
+            // console.log("Token Y Mint:", tokenY.mint.address.toString());
+            // console.log("Token X Decimal:", tokenX.mint.decimals);
+            // console.log("Token Y Decimal:", tokenY.mint.decimals);
+            // console.log("Token X Info:", tokenX_info);
+            // console.log("Token Y Info:", tokenY_info);
 
             // console.log("Position Public Key?:", positionData.publicKey.toString());
-            console.log("Position Data:", positionData);
+            // console.log("Position Data:", positionData);
 
-            const profitX = positionData.feeX.toString();
-            const profitY = positionData.feeY.toString();
-            console.log("Total Fee X:", profitX);
-            console.log("Total Fee Y:", profitY);
-
-            const positionX = positionData.totalXAmount.toString();
-            const positionY = positionData.totalYAmount.toString();
-            console.log("Total Amount X:", positionX);
-            console.log("Total Amount Y:", positionY);
+            const profitX = positionData.feeX.toNumber();
+            const profitY = positionData.feeY.toNumber();
+            const positionX = positionData.totalXAmountExcludeTransferFee.toNumber();
+            const positionY = positionData.totalYAmountExcludeTransferFee.toNumber();
 
             userPortfolio.push({
+                pairName: `${tokenX_symbol}-${tokenY_symbol}`,
                 pairAddress: position.publicKey.toBase58(),
                 apy: pairStats.apy,
                 apr: pairStats.apr,
-                tvl: pairStats.tvl,
                 yield_24h: pairStats.apr,
                 fees_24h: pairStats.fees_24h,
                 volume_24h: pairStats.trade_volume_24h,
-                tokenX: tokenX.mint.address.toString(),
-                tokenY: tokenY.mint.address.toString(),
+                tokenXMint: tokenX.mint.address.toString(),
+                tokenYMint: tokenY.mint.address.toString(),
                 tokenXDecimal: tokenX.mint.decimals,
                 tokenYDecimal: tokenY.mint.decimals,
+                tokenXSymbol: tokenX_info.symbol,
+                tokenYSymbol: tokenY_info.symbol,
+                tokenXPrice: priceX,
+                tokenYPrice: priceY,
+                tokenXLogo: tokenX_info.logo,
+                tokenYLogo: tokenY_info.logo,
                 profitX: profitX,
                 profitY: profitY,
+                profitXUSD: profitX * priceX / 10**tokenX.mint.decimals,
+                profitYUSD: profitY * priceY / 10**tokenY.mint.decimals,
                 positionX: positionX,
                 positionY: positionY,
-                tokenXReserve: pairStats.reserve_x_amount,
-                tokenYReserve: pairStats.reserve_y_amount,
+                positionXUSD: positionX * priceX / 10**tokenX.mint.decimals,
+                positionYUSD: positionY * priceY / 10**tokenY.mint.decimals,
+                reserveX: pairStats.reserve_x_amount,
+                reserveY: pairStats.reserve_y_amount,
+                reserveXUSD: pairStats.reserve_x_amount * priceX / 10**tokenX.mint.decimals,
+                reserveYUSD: pairStats.reserve_y_amount * priceY / 10**tokenY.mint.decimals,
             });
         });
     });
